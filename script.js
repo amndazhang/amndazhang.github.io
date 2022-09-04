e2f84657da6f229239e71b0c8e925546bed523bd
amandazhanglele <amandazhanglele@gmail.com>
2022-09-03 23:39:22 -0700

    Favicon + random image popping up attempt..

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-var i = 0;
-var txt = 'welcome to my website, how are you'; 
-var speed = 50; 
+"use strict";
 
-if (i < txt.length) {
-    document.getElementById("demo").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
+intro.style.textAlign = "center"
+
+function randint(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
 }
+
+function randomLoc(el) {
+    el.style.position = "absolute";
+    el.style.top = randint(0, window.innerHeight) + "px";
+    el.style.left = randint(0, window.innerWidth) + "px";
+}
+
+randomLoc(document.getElementById("star"));
+
